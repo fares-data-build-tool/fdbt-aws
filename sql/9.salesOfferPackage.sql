df1dcd4abefd617522f3aca26b11a30b16c608c4
Danny Davies <danny.davies@infinityworks.com>
2020-07-16 10:54:52 +0100

    Added sql for salesOfferPackage table

---
+++ sql/9.salesOfferPackage.sql
@@ -0,0 +1,18 @@
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
+DROP TABLE IF EXISTS salesOfferPackage;
+
+CREATE TABLE salesOfferPackage(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nocCode` varchar(255) NOT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `purchaseLocation` varchar(255) DEFAULT NULL,
+    `paymentMethod` varchar(255) DEFAULT NULL,
+    `ticketFormat` varchar(255) DEFAULT NULL,
+    INDEX idx_nocCode (nocCode),
+    CONSTRAINT fk_salesOfferPackage_nocTable_nocCode FOREIGN KEY (nocCode) REFERENCES nocTable(nocCode),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARACTER SET = utf8;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
